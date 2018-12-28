b9f427d681beaebcec02249558038066918d52f2
krutik-2-11 <you@example.com>
2018-12-28 15:00:23 +0530

    attendance taking system added

--- face_recognition_knn.py
+++ face_recognition_knn.py
@@ -1,24 +1,4 @@
-"""
-This is an example of using the k-nearest-neighbors (KNN) algorithm for face recognition.
-When should I use this example?
-This example is useful when you wish to recognize a large set of known people,
-and make a prediction for an unknown person in a feasible computation time.
-Algorithm Description:
-The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
-in an unknown image by finding the k most similar faces (images with closet face-features under eucledian distance)
-in its training set, and performing a majority vote (possibly weighted) on their label.
-For example, if k=3, and the three closest face images to the given image in the training set are one image of Biden
-and two images of Obama, The result would be 'Obama'.
-* This implementation uses a weighted vote, such that the votes of closer-neighbors are weighted more heavily.
-Usage:
-1. Prepare a set of images of the known people you want to recognize. Organize the images in a single directory
-   with a sub-directory for each known person.
-2. Then, call the 'train' function with the appropriate parameters. Make sure to pass in the 'model_save_path' if you
-   want to save the model to disk so you can re-use the model without having to re-train it.
-3. Call 'predict' and pass in your trained model to recognize the people in an unknown image.
-NOTE: This example requires scikit-learn to be installed! You can install it with pip:
-$ pip3 install scikit-learn
-"""
+
 
 import math
 from sklearn import neighbors
@@ -39,26 +19,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 currentDate = time.strftime("%d_%m_%y")
 
 def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
-    """
-    Trains a k-nearest neighbors classifier for face recognition.
-    :param train_dir: directory that contains a sub-directory for each known person, with its name.
-     (View in source code to see train_dir example tree structure)
-     Structure:
-        <train_dir>/
-        ├── <person1>/
-        │   ├── <somename1>.jpeg
-        │   ├── <somename2>.jpeg
-        │   ├── ...
-        ├── <person2>/
-        │   ├── <somename1>.jpeg
-        │   └── <somename2>.jpeg
-        └── ...
-    :param model_save_path: (optional) path to save model on disk
-    :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified
-    :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree
-    :param verbose: verbosity of training
-    :return: returns knn classifier that was trained on the given data.
-    """
+    
     X = []
     y = []
 
@@ -100,16 +61,7 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
 
 
 def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
-    """
-    Recognizes faces in given image using a trained KNN classifier
-    :param X_img_path: path to image to be recognized
-    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
-    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
-    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
-           of mis-classifying an unknown person as a known one.
-    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
-        For faces of unrecognized persons, the name 'unknown' will be returned.
-    """
+    
     if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
         raise Exception("Invalid image path: {}".format(X_img_path))
 
@@ -174,7 +126,7 @@ if __name__ == "__main__":
     # STEP 1: Train the KNN classifier and save it to disk
     # Once the model is trained and saved, you can skip this step next time.
     print("Training KNN classifier...")
-    classifier = train("dataset/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
+    #classifier = train("dataset/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
     print("Training complete!")
 
     # STEP 2: Using the trained classifier, make predictions for unknown images
@@ -214,40 +166,11 @@ if __name__ == "__main__":
                     else:
                         i += 1
                         row = get_column_letter(1)
-                print('while ke bahar')
-            print('for ke bahar')
+              
         else:
             print("No workbook found")
                 
-
-
-
-        
-        '''
-        for enrollment, (top, right, bottom, left) in predictions:
-            print("- Found {} at ({}, {})".format(enrollment, left, top))
-            if(os.path.exists('./Attendance.xlsx')):
-                wb = load_workbook(filename = "Attendance.xlsx")
-                sheet = wb.get_sheet_by_name('CseIII')
-                col_index = 4
-                while(True):
-                    col = get_column_letter(col_index)
-                    col = col + '1'
-                    if(sheet[col].value == currentDate):
-                         row = get_column_letter(1)
-                         i = 3
-                         while(True):
-                            row = row + str(i)
-                            if(sheet[row].value == enrollment):
-                                sheet[col + str(i)] = 1
-                                break
-                            i += 1
-                            
-                         break
-                    else:
-                        col_index += 1
-                        
-        '''                     
+                     
         wb.save(filename = "Attendance.xlsx")     
 
         # Display results overlaid on an image
